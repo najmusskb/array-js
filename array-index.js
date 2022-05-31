57a37f592cfbd9c47062ac959844f2cfc579e5ea
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-05-31 21:38:29 +0600

    first commit

---
+++ array-index.js
@@ -0,0 +1,63 @@
+//muloto eikhane indexof diye amra koto number position e ache seta bahir korte pari ..
+
+
+
+var books = ['tablet', 'bolod', 'hablu', 'parasitm', 'finix', 'probitor', 'sargel'];
+var finixIndex = books.indexOf('finix');
+console.log(finixIndex);
+var tabletfind = books.indexOf('tablet');
+console.log(tabletfind);
+
+
+
+
+// kon nam ta kon index e ache seta define kore bahir kora jay 
+
+var secondIndex = books[2];
+console.log(secondIndex);
+
+var secondIndex = books[5];
+console.log(secondIndex);
+
+var secondIndex = books[1];
+console.log(secondIndex);
+
+var secondIndex = books[8];
+console.log(secondIndex);
+
+var secondIndex = books[10];
+console.log(secondIndex);
+
+var secondIndex = books[0];
+console.log(secondIndex);
+
+
+
+
+
+// number er position bahir kora jay mane ulta kag kora jay are ki 
+
+
+
+var numberFind = [1, 2, 3, 4, 5, 6, 7, 8, 9, 454, 57, 411, 25, 3, 44];
+
+var finallyFind = numberFind.indexOf(454);
+console.log(finallyFind);
+
+
+
+
+
+
+
+
+// amra caile position number o change korte par.............ki mojaaaaaaa
+
+var nuMber = [56, 23, 69, 748, 54, 1]
+console.log(nuMber);
+
+nuMber[3] = 26;
+nuMber[0] = 6;
+nuMber[4] = 269;
+nuMber[5] = 22226;
+console.log(nuMber);
---
+++ array.js
@@ -0,0 +1,26 @@
+// eikhan theke amra array declaration sikhlam .jeikhanhe amra array declared korlam .array er modhe koyta element ache seta amra .length diye find out kora sikhlam .
+
+
+
+var friends = [10, 13, 17];
+
+var muricanacur = [34, 40, 50,]
+
+var Naikas = ['naika', 'sabana', 'kopila'];
+
+var oddNumber = [1, 2, 3, 4, 5, 6, 7];
+
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(Naikas.length);
+
+console.log(muricanacur.length);
+
+console.log(friends.length);
+
+console.log(vowels.length);
+
+console.log(oddNumber.length);
+
+
+
