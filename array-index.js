28c2b72c0c00ab3b4c373cf1f053d26887ab4f77
Najmus Sakib <92445715+najmusskb@users.noreply.github.com>
2022-09-16 23:23:25 +0600

    update

--- array-index.js
+++ array-index.js
@@ -1,15 +1,9 @@
 //muloto eikhane indexof diye amra koto number position e ache seta bahir korte pari ..
-
-
-
 var books = ['tablet', 'bolod', 'hablu', 'parasitm', 'finix', 'probitor', 'sargel'];
 var finixIndex = books.indexOf('finix');
 console.log(finixIndex);
 var tabletfind = books.indexOf('tablet');
 console.log(tabletfind);
-
-
-
 
 // kon nam ta kon index e ache seta define kore bahir kora jay 
 
@@ -31,25 +25,12 @@ console.log(secondIndex);
 var secondIndex = books[0];
 console.log(secondIndex);
 
-
-
-
-
 // number er position bahir kora jay mane ulta kag kora jay are ki 
-
-
 
 var numberFind = [1, 2, 3, 4, 5, 6, 7, 8, 9, 454, 57, 411, 25, 3, 44];
 
 var finallyFind = numberFind.indexOf(454);
 console.log(finallyFind);
-
-
-
-
-
-
-
 
 // amra caile position number o change korte par.............ki mojaaaaaaa
 
@@ -60,4 +41,6 @@ nuMber[3] = 26;
 nuMber[0] = 6;
 nuMber[4] = 269;
 nuMber[5] = 22226;
+console.log(nuMber);
+console.log(nuMber);
 console.log(nuMber);
